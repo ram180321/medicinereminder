9ad27da409c1662eaf59d4417f94d4e34984f699
ram180321 <rram65632@gmail.com>
2025-10-14 16:06:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 // Models & scheduler (assumes models/ and scheduler.js are at repo root)
-import Medicine from "./models/Medicine.js";
+import Medicine from "./models/medicine.js";
 import User from "./models/user.js";
 import reminderScheduler from "./scheduler.js";
 
